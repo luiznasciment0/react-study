315681a6cd31ca84b18c3e2a1aafd98ddd38bdb5
Luiz Nascimento <luizp.nascimento@outlook.com>
2020-10-12 16:55:14 -0300

    adding Logo component and tests setup and config

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render a white label by default', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel..
+    // expect - assertion - comparação - análise (espero que renderize a logo branca)
+
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel..
+    // expect - assertion - comparação - análise (espero que renderize a logo branca)
+
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+})
