2740d23373462ce13f93c081a191b7734551076d
Luiz Nascimento <luizp.nascimento@outlook.com>
2020-10-12 17:24:31 -0300

    finishing logo adding tests and responsiveness

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 
@@ -24,5 +25,38 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
     })
+  })
+
+  it('should render a bigger logo', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel..
+    // expect - assertion - comparação - análise (espero que renderize a logo branca)
+
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a normal logo when size is not passed', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel..
+    // expect - assertion - comparação - análise (espero que renderize a logo branca)
+
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel..
+    // expect - assertion - comparação - análise (espero que renderize a logo branca)
+
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(
+      screen.getByLabelText(/Won Games/i).parentElement
+    ).toHaveStyleRule('width', '5.8rem', { media: '(max-width: 768px)' })
   })
 })
