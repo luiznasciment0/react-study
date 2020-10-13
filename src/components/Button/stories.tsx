e81d04a321f2ff0cab039c3e74f6afe7b04f292f
Luiz Nascimento <luizp.nascimento@outlook.com>
2020-10-13 00:04:17 -0300

    last commit of the day

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,32 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { AddShoppingCart } from '@styled-icons/material/AddShoppingCart'
+import Button from '.'
+
+//@styled-icons/material/AddShoppingCart
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: {
+      type: ''
+    }
+  }
+} as Meta
+
+export const Default: Story = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy Now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy now',
+  icon: <AddShoppingCart />
+}
