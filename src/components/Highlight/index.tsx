ae26eb058af39166f4d5b8c82550c31dc1e450c8
Luiz Nascimento <luizp.nascimento@outlook.com>
2020-11-20 23:35:29 -0300

    adding highlight component

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,8 +1,34 @@
 import * as S from './styles'
+import Button from 'components/Button'
 
-const Highlight = () => (
-  <S.Wrapper>
-    <h1>Highlight</h1>
+export type HighlightProps = {
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+  backgroundImage: string
+  floatImage?: string
+  alignment?: 'left' | 'right'
+}
+
+const Highlight = ({
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink,
+  backgroundImage,
+  floatImage,
+  alignment = 'right'
+}: HighlightProps) => (
+  <S.Wrapper backgroundImage={backgroundImage} alignment={alignment}>
+    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
+    <S.Content>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+      <Button as="a" href={buttonLink}>
+        {buttonLabel}
+      </Button>
+    </S.Content>
   </S.Wrapper>
 )
 
