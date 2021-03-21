180c2f80d4de6a260b6031520ab94a9010812131
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-21 19:14:27 -0300

    back to course

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -6,78 +6,84 @@ import { Container } from 'components/Container'
 import Footer from 'components/Footer'
 import Heading from 'components/Heading'
 import Menu from 'components/Menu'
+import GameCardSlider from 'components/GameCardSlider'
+import BannerSlider from 'components/BannerSlider'
 
 import * as S from './styles'
-import BannerSlider from 'components/BannerSlider'
-import GameCardSlider from 'components/GameCardSlider'
 
 export type HomeTemplateProps = {
   banners: BannerProps[]
   newGames: GameCardProps[]
   mostPopularHighlight: HighlightProps
   mostPopularGames: GameCardProps[]
-  upcomingGames: GameCardProps[]
-  upcomingHighlight: HighlightProps
-  upcomingMoreGames: GameCardProps[]
+  upcommingGames: GameCardProps[]
+  upcommingHighligth: HighlightProps
+  upcommingMoreGames: GameCardProps[]
   freeGames: GameCardProps[]
-  freeHighlight: HighlightProps
+  freeHighligth: HighlightProps
 }
 
 const Home = ({
   banners,
   newGames,
-  mostPopularGames,
   mostPopularHighlight,
-  upcomingGames,
-  upcomingHighlight,
-  upcomingMoreGames,
+  mostPopularGames,
+  upcommingGames,
+  upcommingHighligth,
+  upcommingMoreGames,
   freeGames,
-  freeHighlight
+  freeHighligth
 }: HomeTemplateProps) => (
   <section>
     <Container>
       <Menu />
-      <BannerSlider items={banners} />
+      <S.SectionBanner>
+        <BannerSlider items={banners} />
+      </S.SectionBanner>
     </Container>
+
+    <S.SectionNews>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          News
+        </Heading>
+
+        <GameCardSlider items={newGames} color="black" />
+      </Container>
+    </S.SectionNews>
 
     <Container>
-      <Heading lineLeft lineColor="secondary" color="black">
-        News
-      </Heading>
-      <GameCardSlider items={newGames} color="black" />
+      <S.SectionMostPopular>
+        <Heading lineLeft lineColor="secondary">
+          Most Popular
+        </Heading>
+        <Highlight {...mostPopularHighlight} />
+        <GameCardSlider items={mostPopularGames} />
+      </S.SectionMostPopular>
+
+      <S.SectionUpcoming>
+        <Heading lineLeft lineColor="secondary">
+          Upcomming
+        </Heading>
+        <GameCardSlider items={upcommingGames} />
+        <Highlight {...upcommingHighligth} />
+        <GameCardSlider items={upcommingMoreGames} />
+      </S.SectionUpcoming>
+
+      <S.SectionFreeGames>
+        <Heading lineLeft lineColor="secondary">
+          Free games
+        </Heading>
+        <Highlight {...freeHighligth} />
+        <GameCardSlider items={freeGames} />
+      </S.SectionFreeGames>
     </Container>
 
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Most Popular
-      </Heading>
-
-      <Highlight {...mostPopularHighlight} />
-      <GameCardSlider items={mostPopularGames} color="white" />
-    </Container>
-
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Upcomming
-      </Heading>
-
-      <GameCardSlider items={upcomingGames} color="white" />
-      <Highlight {...upcomingHighlight} />
-      <GameCardSlider items={upcomingMoreGames} color="white" />
-    </Container>
-
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Free Games
-      </Heading>
-
-      <Highlight {...freeHighlight} />
-      <GameCardSlider items={freeGames} color="white" />
-    </Container>
-
-    <Container>
-      <Footer />
-    </Container>
+    <S.SectionFooter>
+      <Container>
+        <Footer />
+      </Container>
+    </S.SectionFooter>
   </section>
 )
 
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -13,36 +13,45 @@ const props = {
   newGames: gamesMock,
   mostPopularHighlight: highlightMock,
   mostPopularGames: gamesMock,
-  upcomingGames: gamesMock,
-  upcomingHighlight: highlightMock,
-  upcomingMoreGames: gamesMock,
+  upcommingGames: gamesMock,
+  upcommingHighligth: highlightMock,
+  upcommingMoreGames: gamesMock,
   freeGames: gamesMock,
-  freeHighlight: highlightMock
+  freeHighligth: highlightMock
 }
 
-jest.mock('components/Showcase', () => {
-  return {
-    __esModule: true,
-    default: function Mock() {
-      return <div data-testid="Mock Showcase"></div>
-    }
-  }
-})
-
-jest.mock('components/BannerSlider', () => {
-  return {
-    __esModule: true,
-    default: function Mock() {
-      return <div data-testid="Mock Banner Slider"></div>
-    }
-  }
-})
-
 describe('<Home />', () => {
-  it('should render banner and showcases', () => {
+  it('should render menu and footer', () => {
     renderWithTheme(<Home {...props} />)
 
-    expect(screen.getByTestId('Mock Banner Slider')).toBeInTheDocument()
-    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(5)
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+  })
+
+  it('should render sections', () => {
+    renderWithTheme(<Home {...props} />)
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcomming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render section elements', () => {
+    renderWithTheme(<Home {...props} />)
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
+    // highlight
+    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
 })
