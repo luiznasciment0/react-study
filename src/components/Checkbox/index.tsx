fdab34aa33e95be982fb88162bcd47b1e519cd76
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-22 23:24:41 -0300

    style checkbox

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -12,7 +12,7 @@ const Checkbox = ({
   labelColor = 'white'
 }: CheckboxProps) => (
   <S.Wrapper>
-    <input type="checkbox" id={labelFor} />
+    <S.Input type="checkbox" id={labelFor} />
     {!!label && (
       <S.Label htmlFor={labelFor} labelColor={labelColor}>
         {label}
