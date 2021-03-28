81d923d833042bcdc81d69f298866762cac2e6d1
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-28 17:16:16 -0300

    next link

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -49,6 +49,11 @@ export const BannerContent = styled.div`
     position: relative;
     color: ${theme.colors.white};
     z-index: ${theme.layers.base};
+
+    a {
+      width: fit-content;
+      height: fit-content;
+    }
   `}
 `
 
