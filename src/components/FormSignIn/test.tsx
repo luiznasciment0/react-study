a28d330a215d851c96e030bdb07600793bb9086e
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-28 16:26:23 -0300

    start formsignin

---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignIn from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /Forgot your password?/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text and link to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByRole('link', { name: /Sign up/i })).toBeInTheDocument()
+    expect(screen.getByText(/don't have an account\?/i)).toBeInTheDocument()
+  })
+})
