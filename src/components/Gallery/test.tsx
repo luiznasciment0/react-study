55765f3b54a270b3d83c6a878e76c709d32c6af6
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-06-08 04:05:10 -0300

    advancing gallery

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,16 +1,42 @@
-import { screen } from '@testing-library/react'
 import 'match-media-mock'
+import { screen, fireEvent } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import mock from './mock'
 import Gallery from '.'
-import { renderWithTheme } from 'utils/tests/helpers'
 
 describe('<Gallery />', () => {
-  it('should render the heading', () => {
-    const { container } = renderWithTheme(<Gallery items={mock} />)
+  it('should render thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={mock.slice(0, 2)} />)
 
-    expect(screen.getByLabelText('next image')).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    ).toHaveAttribute('src', mock[0].src)
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    ).toHaveAttribute('src', mock[1].src)
+  })
+
+  it('should handle open/close modal', () => {
+    renderWithTheme(<Gallery items={mock.slice(0, 2)} />)
+    // selecionar o modal
+    const modal = screen.getByLabelText('modal')
+
+    // verificar se o modal tá escondido
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+
+    // clicar no botão de abrir o modal e verificar se ele abriu
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+
+    // clicar no botão de fechar o modal e verificar se ele fechou
+    // fireEvent.click(screen.getByLabelText(/close modal/i))
+    // expect(modal.getAttribute('aria-hidden')).toBe('true')
+    // expect(modal).toHaveStyle({ opacity: 0 })
   })
 })
